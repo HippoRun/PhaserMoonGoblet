8773594930ae3fc502a7afe928fc09ae97eebcc6
HippoRun <mayradlopezh@gmail.com>
2018-09-13 21:06:29 -0600

    state 1 complete

--- States/state0.js
+++ States/state0.js
@@ -1,66 +1,64 @@
 // variable that represents center width and center height of sprite. if you want it on top of left of page make it 0, 0
-var pacman = {}, centerX = 1500 / 2, centerY = 1000 / 2, warewolf, speed = 2;
+var pacman = {}, centerX = 1500 / 2, centerY = 1000 / 2, adam, speed = 6;
 
 pacman.state0 = function(){};
 pacman.state0.prototype = {
-    preload: function(){
+preload: function(){
 // onces we have the images we will add it here
-    game.load.image('warewolf', 'assets/spritesheets/warewolf.png', 240, 370);
-    game.load.image('map', 'assets/backgrounds/map.png');
-    },
-    create: function(){
-    game.physics.startSystem(Phaser.Physics.ARCADE);
+    game.load.spritesheet('adam', 'assets/spritesheets/adamSheet.png', 240, 370);
+    game.load.image('tree', 'assets/backgrounds/TreeBG.png');
+},
+create: function(){
+game.physics.startSystem(Phaser.Physics.ARCADE);
  // this is to change the background in the state. The color has to be a hex color. Hex color picker is a good site for it.
-    game.stage.backgroundColor = '#DDDDDD';
-    console.log('state0');
+game.stage.backgroundColor = '#800080';
+addChangeStateEventListeners();
 
-    addChangeStateEventListeners();
 
 // this will set bounds for the camera to follow. We might not need so code will be commented out.
-// game.world.setBounds(0, 0, 2813, 1000);
+game.world.setBounds(0, 0, 2813, 1000);
 
 // this is to fit all screen sizes
-    game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
+game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
 
 // this wont work until I add the images
-    var map = game.add.sprite(0, 0, 'map1');
-    warewolf = game.add.sprite(centerX, centerY, 'warewolf'); 
-    warewolf.anchor.setTo(0.5, 0.5);
-    warewolf.scale.setTo(0.7, 0.7);
-    game.physics.enable(warewolf);
-    warewolf.body.collideWorldBounds = true;
+    var treeBG = game.add.sprite(0, 0, 'tree');
+    adam = game.add.sprite(centerX, centerY, 'adam'); 
+    adam.anchor.setTo(0.5, 0.5);
+    adam.scale.setTo(0.7, 0.7);
+    game.physics.enable(adam);
+    adam.body.collideWorldBounds = true;
 // walking animation
-    warewolf.animations.add('walk', [0, 1, 2, 3, 4]);
+    adam.animations.add('walk', [0, 1, 2, 3, 4]);
 
 // code bellow is to make the camera move with the character. We might not need or want this so it will be commented out.
-//    game.camera.follow(warewolf);
-//    game.camera.deadzone = new Phaser.Rectangle(ceneterX - 300, 0, 600, 1000);
-
+game.camera.follow(adam);
+game.camera.deadzone = new Phaser.Rectangle(ceneterX - 300, 0, 600, 1000);
 },
     update: function(){
-        if(game.input.keyboard.isDown(Phaser.keyboard.RIGHT)){
-            warewolf.scale.setTo(0.7, 0.7);
-            warewolf.x += speed;
-            warewolf.animations.play('walk', 14, true);
+        if(game.input.keyboard.isDown(Phaser.Keyboard.RIGHT)){
+            adam.scale.setTo(0.7, 0.7);
+            adam.x += speed;
+            adam.animations.play('walk', 14, true);
         }
-        else if(game.input.keyboard.isDown(Phaser.keyboard.LEFT)){
-            warewolf.scale.setTo(-0.7, 0.7);
-            warewolf.x -+ speed;
-            warewolf.animations.play('walk', 14, true);
-        }
-        else{
-            warewolf.animations.stop('walk');
-            warewolf.frame = 0;
-        }
-        if(game.input.keyboard.isDown(Phaser.keyboard.UP)){
-            warewolf.y -= speed;
+    else if(game.input.keyboard.isDown(Phaser.Keyboard.LEFT)){
+            adam.scale.setTo(-0.7, 0.7);
+            adam.x -= speed;
+            adam.animations.play('walk', 14, true);
+    }
+    else{
+            adam.animations.stop('walk');
+            adam.frame = 0;
+    }
+    if(game.input.keyboard.isDown(Phaser.Keyboard.UP)){
+        adam.y -= speed;
 // This is how we will set the bounds for the map need to add the y value after < to set bounds
-            // if(warewolf.y < ){
-//             warewolf.y = 395;
-            //}
+    if(adam.y < 395){
+        adam.y = 395;
+    }
         }
-        else if(game.input.keyboard.isDown(Phaser.keyboard.DOWN)){
-            warewolf.y += speed;
+    else if(game.input.keyboard.isDown(Phaser.Keyboard.DOWN)){
+            adam.y += speed;
         }
     }
 };
@@ -68,12 +66,10 @@ pacman.state0.prototype = {
 function changeState(i, stateNum){
     console.log('state' + stateNum);
     game.state.start('state' + stateNum);
-
 }
 
 function addKeyCallback(key, fn, args){
     game.input.keyboard.addKey(key).onDown.add(fn, null, null, args);
-
 }
 
 function addChangeStateEventListeners(){
@@ -87,5 +83,4 @@ function addChangeStateEventListeners(){
     addKeyCallback(Phaser.keyboard.SEVEN, changeState, 7);
     addKeyCallback(Phaser.keyboard.EIGHT, changeState, 8);
     addKeyCallback(Phaser.keyboard.NINE, changeState, 9);
-
 }
--- States/state1.js
+++ States/state1.js
@@ -1,16 +1,59 @@
 pacman.state1 = function(){};
+
+var cursors, vel = 500, rocks, grass;
+
 pacman.state1.prototype = {
     preload: function(){
 // when map is made save it as field.json
         game.load.tilemap('field', 'assets/tilemaps/field.json', null, Phaser.Tilemap.TILED_JSON);
-        game.load.image('');
+// name the grass tiles grassTiles.png and our rock tiles rocktTles.png
+        game.load.image('grassTiles', 'assets/tilemaps/grassTiles.png');
+        game.load.image('rockTiles', 'assets/tilemaps/rockTiles.png');
+        game.load.image('adam', 'assets/sprites/adam.png');
     },
     create: function(){
  // this is to change the background in the state. The color has to be a hex color. Hex color picker is a good site for it.
+    game.physics.startSystem(Phaser.Physics.ARCADE);
     game.stage.backgroundColor = '#DDDDDD';
-    game.input.keyboard.addKey(Phaser.Keyboard.ONE).onDown.add(changeState, null, null, 1);
+    addChangeStateEventListeners();
 
-    game.input.keyboard.addKey(Phaser.Keyboard.ZERO).onDown.add(changeState, null, null, 0);
+    var map = game.add.tilemap('field');
+    map.addTilesetImage('grassTiles');
+    map.addTilesetImage('rockTiles');
+
+    grass = map.createLayer('grass');
+    rocks = map.createLayer('rocks');
+        
+    map.setCollisionBetween(1, 9, true, 'rocks');
+    map.setCollision(11, true, 'grass');
+
+    adam = game.add.sprite(200, 200, 'adam');
+    adam.scale.setTo(0.2, 0.2);
+    game.physics.enable(adam);
+        
+    cursors = game.input.keyboard.createCursorKeys();
 },
-    update: function(){}
+    update: function(){
+        game.physics.arcade.collide(adam, rocks, function() { console.log('hitting rocks!'); });
+        game.physics.arcade.collide(adam, grass, function(){ console.log('hitting grass!'); });
+    
+    if(cursors.up.isDown){
+        adam.body.velocity.y = -vel;
+    }
+    else if (cursors.down.isDown){
+        adam.body.velocity.y = vel;
+    }
+    else{
+        adam.body.velocity.y = 0;
+    }
+    if(cursors.left.isDown){
+        adam.body.velocity.x = -vel;
+    }
+    else if(cursors.right.isDown){
+        adam.body.velocity.x = vel;
+    }   
+    else{
+        adam.body.velocity.x = 0;
+    }
+    }
 };
