2a9d71d9353cddd4ec0c3af77286a5362a56ebdd
HippoRun <mayradlopezh@gmail.com>
2018-09-14 14:06:53 -0600

    State8 update

--- States/state6.js
+++ States/state6.js
@@ -1,9 +1,33 @@
 pacman.state6 = function(){};
 pacman.state6.prototype = {
-    preload: function(){},
+    preload: function(){
+        game.load.image('volcano', 'assets/sprites/volcano.png');
+        game.load.image('redBall', 'assets/sprites/redBall.png');
+        game.load.image('orBall', 'assets/sprites/orBall.png');
+    },
     create: function(){
 // this is to change the background in the state. The color has to be a hex color. Hex color picker is a good site for it.
-    game.stage.backgroundColor = '#80ff80';
+    game.stage.backgroundColor = '#cc6699';
+    addChangeStateEventListeners();
+        
+    game.add.sprite(centerX, 1000, 'volcano').anchor.setTo(0.5, 1);
+        
+    var emitter = game.add.emitter(centerX, 500, 2000);
+    emitter.makeParticles(['redBall', 'orBall'], 0, 5000, false, true);
+    emitter.maxParticleSpeed.set(300, -300);
+    emitter.maxParticleSPeed.set(-300, -100);
+    emitter.gravity = 300;
+        
+    game.time.events.add(2000, function() {
+        emitter.start(false, 5000, 20);
+        game.time.events.loop(500, function() {
+            if(emitter.on) {
+                emitter.on = false;
+            } else {
+                emitter.on = true;
+            }
+        });
+    });
 },
 
     update: function(){}
--- States/state7.js
+++ States/state7.js
@@ -1,10 +1,46 @@
+var arrow, startPointX, startPointY, endPointX, endPointY, swipeDirection, leeway = 80;
+
 pacman.state7 = function(){};
 pacman.state7.prototype = {
-    preload: function(){},
+    preload: function(){
+        game.load.image('arrow', 'assets/sprites/arrow.png');
+    },
     create: function(){
 // this is to change the background in the state. The color has to be a hex color. Hex color picker is a good site for it.
-    game.stage.backgroundColor = '#80ff80';
+    game.stage.backgroundColor = '#a6ff4d';
+    addChangeStateEventListeners();
+        
+    arrow = game.add.s;rite(centerX, centerY, 'arrow');
+    arrow.anchor.setTo(0.5);
+        
+    game.input.onDown.add(this.startSwipe);
+    game.input.onUp.add(this.getSwipeDirection);
 },
-
-    update: function(){}
+startSwipe: function(){
+    startPointX = game.input.x;
+    startpointY = game.input.y;
+},
+getSwipeDirection: function() {
+    endPointX = game.input.x;
+    endPointY = game.input.y;
+    
+    if (Math.abs(endPointY - startPointY) < leeway && Math.abs(endPointX - startPointX) < leeway) {
+        return false;
+    }
+    
+    if (Math.abs(endPointY - startPointY) < Math.abs (endPointX - startPointX)) {
+        console.log('horizontal');
+        if (endPointX > startPointX) {
+            swipeDirection = 90;
+        } else {
+            console.log('vertical');
+            if (endPointY > startPointY) {
+                swipeDirection = 180;
+            } else {
+                swipeDirection = 0;
+            }
+        }
+        
+        arrow.angle = swipeDirection;
+    }
 };
